fb7cc198692abe8044376767365eb58224849b12
Miguel <miguelangelbernal09@gmail.com>
2025-05-16 09:24:33 -0500

    Create User Creation and Addition of Icons

---
+++ Pages/Users/Create.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Users
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        [BindProperty]
+        public User User { get; set; } = default!;
+        public List<SelectListItem> RoleOptions { get; } = new()
+    {
+        new SelectListItem { Value = "User", Text = "User" },
+        new SelectListItem { Value = "Admin", Text = "Admin" }
+    };
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Users == null || User == null)
+            {
+                return Page();
+            }
+            _context.Users.Add(User);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+        }
+
+
+    }
+}
